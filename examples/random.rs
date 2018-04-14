c7ebae1df9d0b25f41cc7a6004b96a6a551811e5
Alex Jago <4475543+alexjago@users.noreply.github.com>
2018-04-15 01:23:23 +1000

    Initial version of Blinkt_Spidev

--- examples/random.rs
+++ examples/random.rs
@@ -1,15 +1,15 @@
-extern crate blinkt;
+extern crate blinkt_spidev;
 extern crate rand;
 
 use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
-use blinkt::Blinkt;
+use blinkt_spidev::BlinktSpidev;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut blinkt = Blinkt::new().unwrap();
+    let mut blinkt = BlinktSpidev::new().unwrap();
 
     blinkt.set_all_pixels_brightness(0.1);
 
